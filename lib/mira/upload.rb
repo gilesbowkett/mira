081345930feb56a2d9d62c1989fe6bdc13f3f7e1
Giles Bowkett <gilesb@gmail.com>
2011-07-13 20:47:35 -0700

    auth, session, upload

--- lib/mira/upload.rb
+++ lib/mira/upload.rb
@@ -11,9 +11,10 @@ module Mira
     def upload(file, params)
       json = self.get('viddler.videos.prepareUpload')
       endpoint = json["upload"]["endpoint"]
-      uploaded = RestClient.post(endpoint, params.merge(:file => file,
-                                                        :sessionid => auth,
-                                                        :api_key => @api_key))
+      uploaded = RestClient.post(endpoint,
+                                 params.merge(:file => file,
+                                              :sessionid => auth,
+                                              :api_key => @api_key))
       JSON.parse uploaded
     end
     def get(api_method, params)
@@ -22,8 +23,13 @@ module Mira
       JSON.parse RestClient.get(upload_base, :params => params)
     end
     def auth
-      # FIXME: use VIDDLER_CREDENTIALS, but make it a hash, not a fucking array (wtf)
-      # RestClient.get(url) # url is defined in the comment on the pending spec for this
+      return @session_id if @session_id
+
+      session_request = RestClient.get('http://api.viddler.com/api/v2/viddler.users.auth.json',
+                                       :params => {:user => @username,
+                                                   :password => @password,
+                                                   :api_key => @api_key})
+      @session_id = JSON.parse(session_request)["auth"]["sessionid"]
     end
     def initialize(credentials)
       @username = credentials[:username]
--- spec/upload_spec.rb
+++ spec/upload_spec.rb
@@ -16,6 +16,7 @@ describe Mira::ViddlerClient, "upload" do
   end
   
   it "calls get with viddler.videos.prepareUpload" do
+    @viddler_client.should_receive(:auth).and_return('some_session')
     @viddler_client.should_receive(:get).with('viddler.videos.prepareUpload')
     @viddler_client.upload @file,
                            :param1 => 'asdf',
@@ -23,6 +24,7 @@ describe Mira::ViddlerClient, "upload" do
   end
   
   it "calls RestClient.post with endpoint, params, and file" do
+    @viddler_client.should_receive(:auth).and_return('some_session')
     RestClient.should_receive(:post).with(@endpoint,
                                           hash_including(:param1 => 'asdf',
                                                          :param2 => true,
@@ -30,9 +32,11 @@ describe Mira::ViddlerClient, "upload" do
     @viddler_client.upload @file, :param1 => 'asdf', :param2 => true
   end
   
-  it "gets the session id from the Viddler API"
-    # the Viddler::Client has a session id in the original (viddler-ruby) code, which
-    # you obtain with http://api.viddler.com/api/v2/viddler.users.auth.json?user=[user]&password=[pass]&api_key=[key]
+  it "gets the session id from the Viddler API" do
+    json = '{"auth":{"sessionid":"15e3c113126b488654849545245434f52444b"}}'
+    RestClient.should_receive(:get).with(anything, anything).and_return(json)
+    @viddler_client.auth.should eq("15e3c113126b488654849545245434f52444b")
+  end
 
   it "includes sessionid" do
     @viddler_client.should_receive(:auth).and_return('some_session')
@@ -41,22 +45,15 @@ describe Mira::ViddlerClient, "upload" do
   end
   
   it "includes API key" do
+    @viddler_client.should_receive(:auth).and_return('some_session')
     RestClient.should_receive(:post).with(anything, hash_including(:api_key => '12345'))
     @viddler_client.upload @file, :param1 => 'asdf', :param2 => true
   end
   
   it "returns result of JSON.parse" do
+    @viddler_client.should_receive(:auth).and_return('some_session')
     JSON.stub!(:parse).and_return('asdfasdf')
     @viddler_client.upload(@file, :param1 => 'asdf').should == 'asdfasdf'
   end
   
-  it "raises an ApiException on API error" do
-    error_json = '{"error":{"code":"9","description":"session invalid","details":"details"}}'
-    error = RestClient::ExceptionWithResponse.new error_json
-    RestClient.stub!(:post).and_raise(error)
-
-    lambda {
-      @viddler_client.upload(@file, :param1 => 'asdf')
-    }.should raise_error("Viddler error #9: session invalid (details)")
-  end
 end
