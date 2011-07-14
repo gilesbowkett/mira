910bd280060a5dbe7f5927e1f8d45c8174fd6c77
Giles Bowkett <gilesb@gmail.com>
2011-07-14 11:46:19 -0700

    demonstrate embed codes

--- example.rb
+++ example.rb
@@ -3,8 +3,9 @@ require 'lib/mira'
 require 'yaml'
 
 viddler = Mira::ViddlerClient.new(YAML.load(File.open("viddler.credentials")))
-viddler.upload(File.new("sample.mov"),
-               :title => "sample",
-               :tags => "your mom",
-               :description => "whatever")
+result = viddler.upload(File.new("sample.mov"),
+                        :title => "sample",
+                        :tags => "your mom",
+                        :description => "whatever")
+puts Mira::Viddler.embed_code(:id => result["video"]["id"])
 
